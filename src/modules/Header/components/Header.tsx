764ff8f5de286c300e3f881d88e8044af64b75f3
Дмитрий <brepex@MacBook-Air-Dmitrij.local>
2023-05-29 21:08:01 +0700

    i implemented the website header without mobile menu adaptation

--- src/modules/Header/components/Header.tsx
+++ src/modules/Header/components/Header.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
-        <header>
+        <header className={`py-4`}>
             <div className={`container mx-auto`}>
-                test
+                <div className={`flex items-center justify-between`}>
+                    <div className={`flex items-center`}>
+                        <NavLink
+                            to={'/project'}
+                            className={({isActive}) => `${isActive && 'text-custom_brown'} text-2xl hover:text-colors-gray-500 duration-300 font-bold mr-4`}
+                        >Об проекте</NavLink>
+
+                        <NavLink
+                            to={'/about_me'}
+                            className={({isActive}) => `${isActive && 'text-custom_brown'} text-2xl hover:text-colors-gray-500 duration-300 font-bold mr-4`}
+                        >Обо мне</NavLink>
+
+                        <NavLink
+                            to={'/'}
+                            className={({isActive}) => `${isActive && 'text-custom_brown'} text-2xl hover:text-colors-gray-500 duration-300 font-bold`}
+                        >Список постов</NavLink>
+
+                    </div>
+
+                    <div>
+                        <div className={`text-xl block text-end font-bold`}>Дмитрий Путилов Андреевич</div>
+                        <a className={`text-2xl block text-end text-custom_red font-normal`} href="tel::_79059818973">+7 905 981-89-73</a>
+                    </div>
+                </div>
             </div>
         </header>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      custom_brown: "#bb946b",
+      custom_red: "#eb5f67",
+      colors
+    },
     extend: {},
   },
   plugins: [],
